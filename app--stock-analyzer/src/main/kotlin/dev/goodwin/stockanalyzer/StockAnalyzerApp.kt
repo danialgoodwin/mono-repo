1ae6b674e4c44fab10f3fe8cbb214476b22025fd
Danial Goodwin <danialgoodwin@gmail.com>
2021-09-27 20:26:54 -0700

    Add placeholder for app--stock-analyzer

---
+++ app--stock-analyzer/src/main/kotlin/dev/goodwin/stockanalyzer/StockAnalyzerApp.kt
@@ -0,0 +1,6 @@
+package dev.goodwin.stockanalyzer
+
+// Idea: Desktop app that concurrently shows 5y-view, 1y-view, 30d-view, 7d-view, and 1d-view
+// - Find good times to invest for a 10% gain
+class StockAnalyzerApp {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "mono-repo"
 include("learn--interviews")
 include("learn--project-euler")
 include("game--snake")
+include("app--stock-analyzer")
